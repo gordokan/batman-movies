73635afb7089c2e54b54711369a35f0c70d7e4da
Kandace Gordon <kandace.gordon@Salespad.com>
2019-10-12 13:57:27 -0400

    update styles

--- scripts/movie.component.ts
+++ scripts/movie.component.ts
@@ -3,23 +3,31 @@ import { VerboseUiMovieModel } from "./models/ui-movie.model";
 // import images from "../content/images/*"; // https://github.com/parcel-bundler/parcel/issues/317
 
 class controller {
-  public static readonly $inject = ["$scope"];
+  public static readonly $inject = ["$scope", "$window"];
   protected movieData: VerboseUiMovieModel;
   private $scope;
-  constructor($scope) {
+  private $window;
+  constructor($scope, $window) {
     this.$scope = $scope;
+    this.$window = $window;
   }
-  $onInit() {
+  protected $onInit() {
     // TODO show a loading icon?
   }
 
-  $onChanges(changes) {
+  protected $onChanges(changes) {
     if (changes && changes.movieId && changes.movieId.currentValue) {
       // TODO complete this check
       getMovieById(changes.movieId.currentValue).then(movie => {
         this.$scope.$applyAsync(() => (this.movieData = movie));
       });
     }
+  }
+
+  protected openLink(link: string) {
+    if (!link) return;
+
+    this.$window.open(link, "_blank");
   }
 }
 export const movieComponent = {
@@ -29,17 +37,17 @@ export const movieComponent = {
   controller,
   template: `
     <div class="movie" ng-if="$ctrl.movieData">
-      <img class="movie-image" ng-src="{{$ctrl.movieData.posterUrl}}"/>
-      <div class="movie-details-container">
-        <h3>{{$ctrl.movieData.title}}</h3>
-        <div class="movie-details">
-          <div class="movie-details-rating">{{$ctrl.movieData.rating}}</div>
-          <div class="movie-details-runTime">{{$ctrl.movieData.runTime}}</div>
-          <div class="movie-details-released">{{$ctrl.movieData.released}}</div>
+      <img class="movie__image" ng-src="{{$ctrl.movieData.posterUrl}}"/>
+      <div class="movie__overview-container">
+        <h2 class="movie__title">{{$ctrl.movieData.title}}</h2>
+        <div class="movie__details">
+          <div class="movie__rated">{{$ctrl.movieData.rated}}</div>
+          <div class="movie__runtime">{{$ctrl.movieData.runtime}}</div>
+          <div class="movie__released">{{$ctrl.movieData.released}}</div>
         </div>
-        <div class="movie-plot">{{$ctrl.movieData.plot}}</div>
+        <div class="movie__plot">{{$ctrl.movieData.plot}}</div>
+        <button class="movie__external-button"ng-click="$ctrl.openLink($ctrl.movieData.externalLink)">View on IMDB</button>
       </div>
-      <a href="{{$ctrl.movieData.externalLink}}" target="_blank">View on IMDB</a>
     </div>
   `
 };
